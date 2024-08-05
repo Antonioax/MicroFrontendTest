7e5b85e848d3a70ef18b35d7091ef07f477f9801
Antonio Jurić <juricantonio18@gmail.com>
2024-08-05 11:33:42 +0200

    Added barchart microfrontend to a container microfrontend

--- container/webpack.config.js
+++ container/webpack.config.js
@@ -1,14 +1,21 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
   devServer: {
-    port: 3002,
+    port: 3000,
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new ModuleFederationPlugin({
+      name: "container",
+      remotes: {
+        barchart: "barchart@http://localhost:3001/remoteEntry.js"
+      }
+    })
   ],
   watch: true,
 };
